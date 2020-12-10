eb83a2af5c95a616d373fdca673ec1e7571787a6
Ryan Manseau <rmanseau@arista.com>
2020-12-10 12:03:42 -0800

    swapped g; and gc, cleaned up file

--- config.el
+++ config.el
@@ -3,11 +3,10 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "Ryan Manseau"
-      user-mail-address "ryan@arista.com")
+;; (setq user-full-name "Ryan Manseau"
+;;       user-mail-address "ryan@arista.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -25,7 +24,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-molokai)
+;; (setq doom-theme 'doom-molokai)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -33,7 +32,7 @@
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type nil)
+;; (setq display-line-numbers-type nil)
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
@@ -57,19 +56,16 @@
 
 (load! "arista.el" doom-private-dir t)
 
-;; misc
+;; look / feel
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(setq doom-theme 'doom-molokai)
+(setq display-line-numbers-type nil)
+(setq enable-recursive-minibuffers nil)
 (unless window-system
   (xterm-mouse-mode t)
   (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
   (global-set-key (kbd "<mouse-5>") 'scroll-up-line))
-
-(setq enable-recursive-minibuffers nil)
-
-
-;; after package config
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (after! avy
   (setq avy-all-windows t)
@@ -78,10 +74,15 @@
   (set-face-attribute 'avy-lead-face-0 nil :background "red" :foreground "white")
   (set-face-attribute 'avy-lead-face-1 nil :background "red" :foreground "white")
   (set-face-attribute 'avy-lead-face-2 nil :background "red" :foreground "white"))
+
 (after! swiper
   (set-face-attribute 'swiper-line-face nil :background "color-240" :foreground 'unspecified ))
+
 (after! ivy
-  (setq ivy-use-virtual-buffers t))
+  (setq ivy-use-virtual-buffers t)
+  (set-face-attribute 'ivy-modified-buffer nil :foreground 'unspecified :inherit font-lock-doc-face )
+  )
+
 (after! evil
   (setq evil-move-cursor-back nil)
   (setq evil-cross-lines t))
@@ -89,26 +90,20 @@
 ;; keybinds
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(map!
- :leader
- (:prefix "w" ; window
-  "-" #'evil-window-split
-  "\\" #'evil-window-vsplit
+; leader keys
+(map! :leader
+      (:prefix "w" ; window
+       "-" #'evil-window-split
+       "\\" #'evil-window-vsplit
+       "+" nil)
+      :desc "M-x" ";" #'counsel-M-x
+      :desc "Eval expression" ":" #'pp-eval-expression)
 
-  "+" nil
-  )
- :desc "M-x" ";" #'counsel-M-x
- :desc "Eval expression" ":" #'pp-eval-expression
- )
-
+; buffer / window management
 (map! :map ivy-minibuffer-map "C-M-k" #'ivy-switch-buffer-kill)
-
- ; window / mode nav
 (map! :nmiv "C-o" #'evil-window-next
       (:map compilation-mode-map "C-o" nil)
-      (:map help-mode-map :n "C-o" nil)
-      )
-
+      (:map help-mode-map :n "C-o" nil))
 (map! :nmvg "C-b" #'ivy-switch-buffer
       (:map magit-mode-map :nv "C-b" nil)
       (:map counsel-find-file-map "C-b"
@@ -117,7 +112,6 @@
          (ivy-exit-with-action
           (lambda (_)
             (ivy-switch-buffer))))))
-
 (map! :nmvg "C-f" #'counsel-find-file
       (:map magit-mode-map :nv "C-f" nil)
       (:map ivy-switch-buffer-map "C-f"
@@ -131,7 +125,7 @@
                          default-directory)))
               (counsel-find-file)))))))
 
-
+; edit mode
 (map! :iv "C-g" #'evil-force-normal-state)
 (map! :iv "C-j" #'evil-force-normal-state)
 
@@ -144,21 +138,24 @@
 (map! :nmv "K" (kbd "4k"))
 (map! :nmv "L" #'evil-forward-WORD-end)
 (map! :nmv "H" #'evil-backward-WORD-begin)
-
 (map! :mnv "ga" #'evil-avy-goto-char-2)
 (map! :mnv "g/" #'evil-avy-goto-char-timer)
 (map! :mnv "gl" #'evil-avy-goto-line)
+(map! :n "gb" #'better-jumper-jump-backward)
+(map! :n "gf" #'better-jumper-jump-forward)
+(map! :nmv "gc" #'goto-last-change)
+
+; editing
+(map! :nmv "gj" #'evil-join)
+(map! :nmv "g;" #'comment-line)
+(map! :n "U" #'evil-redo)
+
+; search
+(map! :n "/" #'swiper)
+(map! :n "?" #'swiper-all)
+(map! :n "*" #'swiper-thing-at-point)
 (map! :mnv "C-_" #'+default/search-project
       :mnv "C-/" #'+default/search-project
       (:map undo-fu-mode-map
       "C-/" nil
       "C-_" nil))
-(map! :mnv "C-l" #'evil-avy-goto-line)
-(map! :n "gb" #'better-jumper-jump-backward)
-(map! :n "gf" #'better-jumper-jump-forward)
-
-; utilities
-(map! :n "/" #'swiper)
-(map! :n "?" #'swiper-all)
-(map! :n "*" #'swiper-thing-at-point)
-(map! :n "U" #'evil-redo)
