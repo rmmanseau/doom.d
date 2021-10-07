76fd38130b07887bc3b28d6d7e6659f78191f726
Ryan Manseau <rmanseau@arista.com>
2021-10-07 15:54:52 -0700

    web mode indentation & rjsx-mode by default

--- config.el
+++ config.el
@@ -141,6 +141,19 @@
   '(org-date :foreground "white" :background "#101010" )
   '((org-tag org-tag-group) :foreground "#9c91e4"))
 
+  (defun setup-web-indent (n)
+    ;; web development
+    (setq javascript-indent-level n) ; javascript-mode
+    (setq js-indent-level n) ; js-mode
+    (setq js2-basic-offset n) ; js2-mode, alias of js-indent-level
+    (setq web-mode-markup-indent-offset n) ; web-mode, html tag in html file
+    (setq web-mode-css-indent-offset n) ; web-mode, css in html file
+    (setq web-mode-code-indent-offset n) ; web-mode, js code in html file
+    (setq css-indent-offset n) ; css-mode
+    )
+  (setup-web-indent 2)
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
+
 ;; keybinds
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
