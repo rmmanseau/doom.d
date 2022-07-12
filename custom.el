345ce42d15fa4343e247cbe3c666e13e0806920d
Ryan Manseau <rmanseau@arista.com>
2022-07-12 11:52:47 -0700

    add branch section to magit status

---
+++ custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-status-sections-hook
+   '(magit-insert-status-headers magit-insert-local-branches magit-insert-merge-log magit-insert-rebase-sequence magit-insert-am-sequence magit-insert-sequencer-sequence magit-insert-bisect-output magit-insert-bisect-rest magit-insert-bisect-log magit-insert-untracked-files magit-insert-unstaged-changes magit-insert-staged-changes magit-insert-stashes magit-insert-unpushed-to-pushremote magit-insert-unpushed-to-upstream-or-recent magit-insert-unpulled-from-pushremote magit-insert-unpulled-from-upstream)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(avy-goto-char-timer-face ((t (:background "red" :foreground nil))))
+ '(avy-lead-face ((t (:background "red" :foreground "white"))))
+ '(avy-lead-face-0 ((t (:background "red" :foreground "white"))))
+ '(avy-lead-face-1 ((t (:background "red" :foreground "white"))))
+ '(avy-lead-face-2 ((t (:background "red" :foreground "white"))))
+ '(eros-result-overlay-face ((t (:background nil))))
+ '(ivy-modified-buffer ((t (:foreground nil :inherit font-lock-doc-face))))
+ '(show-paren-match ((t (:background "brightblack" :foreground nil))))
+ '(swiper-line-face ((t (:background "color-235" :foreground nil)))))
