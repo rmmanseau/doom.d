909beb439fa2ea7d65ae86401b46cad88f6ab08c
Ryan Manseau <ryan@manseau.dev>
2022-07-12 19:06:37 -0700

    added lsp for yaml

--- init.el
+++ init.el
@@ -167,7 +167,7 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        web                 ; the tubes
-       yaml              ; JSON, but readable
+       (yaml +lsp)              ; JSON, but readable
 
        :email
        ;;(mu4e +gmail)
